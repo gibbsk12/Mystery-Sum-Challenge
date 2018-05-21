62065792233b659b3997998f30028ba1f820c2b1
Kate Gibbs <kategibbs@Kates-MacBook-Pro-2.local>
2018-05-21 09:23:03 -0400

    gem clicks are working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,61 @@
 
-$(document).ready(function(){
-//Sets up variables
-var generatedNumber = 0;
-var playerSum = 0;
-var greenNumber = 0;
-var greenMultiplier = 0;
-var redNumber = 0;
-var redMultiplier = 0; 
-var blueNumber = 0;
-var blueMultiplier = 0;
-var orangeNumber = 0;
-var orangeMultiplier = 0; 
-var winTotal = 0;
-var lossTotal = 0;
+$(document).ready(function () {
+   
+    //Sets up variables
+    var generatedNumber = 0;
+    var playerSum = 0;
+    var greenNumber = 0;
+    var greenMultiplier = 0;
+    var redNumber = 0;
+    var redMultiplier = 0;
+    var blueNumber = 0;
+    var blueMultiplier = 0;
+    var orangeNumber = 0;
+    var orangeMultiplier = 0;
+    var winTotal = 0;
+    var lossTotal = 0;
 
+    //Generates random variables 
+    var greenNumber = Math.floor(Math.random() * 10)+1;
+    var redNumber = Math.floor(Math.random() * 10)+1;
+    var orangeNumber = Math.floor(Math.random() * 10)+1;
+    var blueNumber = Math.floor(Math.random() * 10)+1;
+    var greenMultiplier = Math.floor(Math.random() * 5)+1;
+    var redMultiplier = Math.floor(Math.random() * 5)+1;
+    var blueMultiplier = Math.floor(Math.random() * 5)+1;
+    var orangeMultiplier = Math.floor(Math.random() * 5)+1;
 
+    //Generates magicNumber
+    var generatedNumber = greenNumber * greenMultiplier + redNumber * redMultiplier + orangeNumber * orangeMultiplier + blueNumber * blueMultiplier;
+    console.log(generatedNumber);
 
-//Generates random variables 
-var greenNumber = Math.floor(Math.random() * 11);
-var redNumber = Math.floor(Math.random() * 11);
-var orangeNumber = Math.floor(Math.random() * 11);
-var blueNumber = Math.floor(Math.random() * 11);
-var greenMultiplier = Math.floor(Math.random() * 6);
-var redMultiplier = Math.floor(Math.random() * 6);
-var blueMultiplier = Math.floor(Math.random() * 6);
-var orangeMultiplier = Math.floor(Math.random() * 6);
+    $("#magicNumber").text(generatedNumber);
+    $("#playerTotal").text(playerSum);
 
-//Generates magicNumber
-var generatedNumber = greenNumber*greenMultiplier + redNumber*redMultiplier + orangeNumber*orangeMultiplier + blueNumber*blueMultiplier;
-console.log(generatedNumber);
+    //When a player clicks a gem, the gemNumber will be added to player total.
+    $('.blue').on('click', function () {
+        playerSum = playerSum + blueNumber;
+        console.log("New playerTotal= " + playerTotal);
+        $('#playerTotal').text(playerSum);
+    });
 
-$("#magicNumber").text(generatedNumber);
-$("#playerTotal").text(playerSum);
+    $('.green').on('click', function () {
+        playerSum = playerSum + greenNumber;
+        console.log("New playerTotal= " + playerTotal);
+        $('#playerTotal').text(playerSum);
+    });
+
+    $('.red').on('click', function () {
+        playerSum = playerSum + redNumber;
+        console.log("New playerTotal= " + playerTotal);
+        $('#playerTotal').text(playerSum);
+    });
+
+    $('.orange').on('click', function () {
+        playerSum = playerSum + orangeNumber;
+        console.log("New playerTotal= " + playerTotal);
+        $('#playerTotal').text(playerSum);
+    });
+
 
 });
-
-// $(".green").on ("click", function(){
-//     var NewTotal = playerSum + greenNumber;
-//     console.log(newTotal);
-//     $("#playerTotal").text(newTotal);
-// });
-
-
-$(".green").on ("click", function(){
-    playerSum = playerSum + greenNumber;
-    console.log("New playerTotal= " + playerSum);
-    $('#playerTotal').text(playerSum); });
